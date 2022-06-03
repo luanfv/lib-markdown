3777b17e7cf4c38f01be51e9f5daa43c1f7686aa
luanfv <luanfrancavieira@gmail.com>
2022-06-03 10:47:19 -0300

    get file text

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(`${chalk.red('Hello')} ${chalk.blue('World!')}`);
+function handleError(error) {
+    throw new Error(chalk.red(error, '\nArquivo não encontrado!'));
+}
+
+function getFile(filepath) {
+    const encoding = 'utf-8';
+
+    fs.readFile(filepath, encoding, (error, text) => {
+        if (error) {
+            handleError(error);
+        }
+
+        console.log(chalk.green(text));
+    });
+}
+
+console.log(getFile('./files/text1.md'));
